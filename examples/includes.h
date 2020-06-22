6a17fe267e82afc47bf87687f7ff50c6ab8b5e35
Oblivious-Oblivious <me262thanos@gmail.com>
2020-06-22 22:56:18 +0300

    Added compile files.

---
+++ examples/includes.h
@@ -0,0 +1,9 @@
+#ifndef __INCLUDES_H_
+#define __INCLUDES_H_
+
+#include <stdlib.h> /* malloc, calloc, realloc, free */
+#include <setjmp.h> /* jmp_buf, setjmp */
+#include <stdarg.h> /* va_start, va_end, va_arg */
+#include <signal.h> /* singal, kill */
+
+#endif
