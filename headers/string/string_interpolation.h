715c4be473294421429d2f753d6572e84ce4f94c
Oblivious-Oblivious <me262thanos@gmail.com>
2020-07-09 19:11:47 +0300

    Update string_interpolation.h

--- headers/string/string_interpolation.h
+++ headers/string/string_interpolation.h
@@ -2,10 +2,6 @@
 #include "string.h"
 #include "string_split.h"
 
-#define PP_NARG(...) \
-    PP_NARG_(__VA_ARGS__,PP_RSEQ_N())
-#define PP_NARG_(...) \
-    PP_256TH_ARG(__VA_ARGS__)
 #define PP_256TH_ARG( \
     _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
     _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
@@ -60,6 +56,11 @@
     29,28,27,26,25,24,23,22,21,20, \
     19,18,17,16,15,14,13,12,11,10, \
     9,8,7,6,5,4,3,2,1,0
+    
+#define PP_NARG_(...) \
+    PP_256TH_ARG(__VA_ARGS__)
+#define PP_NARG(...) \
+    PP_NARG_(__VA_ARGS__,PP_RSEQ_N())
 
 #define puts(...) display(PP_NARG(__VA_ARGS__), __VA_ARGS__)
 
